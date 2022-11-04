e603a38ada1c8a6708fa15511caef06b2c2c42d8
Resh <85751727+reshmipradeep@users.noreply.github.com>
2022-11-04 09:04:07 +0530

    Add files via upload

---
+++ PES2UG20CS270.py
@@ -0,0 +1,143 @@
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+
+"""
+Use DecisionTreeClassifier to represent a stump.
+------------------------------------------------
+DecisionTreeClassifier Params:
+    critereon -> entropy
+    max_depth -> 1
+    max_leaf_nodes -> 2
+Use the same parameters
+"""
+# REFER THE INSTRUCTION PDF FOR THE FORMULA TO BE USED 
+
+class AdaBoost:
+
+    """
+    AdaBoost Model Class
+    Args:
+        n_stumps: Number of stumps (int.)
+    """
+
+    def __init__(self, n_stumps=20):
+
+        self.n_stumps = n_stumps
+        self.stumps = []
+
+    def fit(self, X, y):
+        """
+        Fitting the adaboost model
+        Args:
+            X: M x D Matrix(M data points with D attributes each)(numpy float)
+            y: M Vector(Class target for all the data points as int.)
+        Returns:
+            the object itself
+        """
+        self.alphas = []
+
+        sample_weights = np.ones_like(y) / len(y)
+        for _ in range(self.n_stumps):
+
+            st = DecisionTreeClassifier(
+                criterion='entropy', max_depth=1, max_leaf_nodes=2)
+            st.fit(X, y, sample_weights)
+            y_pred = st.predict(X)
+
+            self.stumps.append(st)
+
+            error = self.stump_error(y, y_pred, sample_weights=sample_weights)
+            alpha = self.compute_alpha(error)
+            self.alphas.append(alpha)
+            sample_weights = self.update_weights(y, y_pred, sample_weights, alpha)
+
+        return self
+
+    def stump_error(self, y, y_pred, sample_weights):
+        """
+        Calculating the stump error
+        Args:
+            y: M Vector(Class target for all the data points as int.)
+            y_pred: M Vector(Class target predicted for all the data points as int.)
+            sample_weights: M Vector(Weight of each sample float.)
+        Returns:
+            The error in the stump(float.)
+        """
+
+        # TODO
+        error = 0
+        for i in range(len(y)):
+            if(y[i] != y_pred[i]):
+                error += sample_weights[i]
+        return error
+
+    def compute_alpha(self, error):
+        """
+        Computing alpha
+        The weight the stump has in the final prediction
+        Use eps = 1e-9 for numerical stabilty.
+        Args:
+            error:The stump error(float.)
+        Returns:
+            The alpha value(float.)
+        """
+        eps = 1e-9
+        # TODO
+        alpha = 0.5*np.log((1-error)/(error+eps))
+        return alpha
+        
+
+    def update_weights(self, y, y_pred, sample_weights, alpha):
+        """
+        Updating Weights of the samples based on error of current stump
+        The weight returned is normalized
+        Args:
+            y: M Vector(Class target for all the data points as int.)
+            y_pred: M Vector(Class target predicted for all the data points as int.)
+            sample_weights: M Vector(Weight of each sample float.)
+            alpha: The stump weight(float.)
+        Returns:
+            new_sample_weights:  M Vector(new Weight of each sample float.)
+        """
+
+        # TODO
+        new_sample_weights = []
+        for i in range(len(sample_weights)):
+            if(y[i] == y_pred[i]):
+                wt = (sample_weights[i]) * (np.exp(-alpha))
+            else:
+                wt = (sample_weights[i]) * (np.exp(alpha))
+            new_sample_weights.append(wt)
+        new_sample_weights /= sum(new_sample_weights)
+        return new_sample_weights
+
+    def predict(self, X):
+        """
+        Predicting using AdaBoost model with all the decision stumps.
+        Decison stump predictions are weighted.
+        Args:
+            X: N x D Matrix(N data points with D attributes each)(numpy float)
+        Returns:
+            pred: N Vector(Class target predicted for all the inputs as int.)
+        """
+        # TODO
+        arr = np.array([self.stumps[stump].predict(X) for stump in range(self.n_stumps)])
+        pred = np.sign(arr[0])
+        return pred
+
+    def evaluate(self, X, y):
+        """
+        Evaluate Model on test data using
+            classification: accuracy metric
+        Args:
+            x: Test data (N x D) matrix
+            y: True target of test data
+        Returns:
+            accuracy : (float.)
+        """
+        pred = self.predict(X)
+        # find correct predictions
+        correct = (pred == y)
+
+        accuracy = np.mean(correct) * 100  # accuracy calculation
+        return accuracy
