6549dd9ecfc0630ebb78372fb2a46b744ffd8bf2
Resh <85751727+reshmipradeep@users.noreply.github.com>
2022-12-01 12:43:35 +0530

    Add files via upload

---
+++ PES2UG20CS270.py
@@ -0,0 +1,116 @@
+import numpy as np
+
+
+class KMeansClustering:
+    """
+    K-Means Clustering Model
+
+    Args:
+        n_clusters: Number of clusters(int)
+    """
+
+    def __init__(self, n_clusters, n_init=10, max_iter=1000, delta=0.001):
+
+        self.n_cluster = n_clusters
+        self.n_init = n_init
+        self.max_iter = max_iter
+        self.delta = delta
+
+    def init_centroids(self, data):
+        idx = np.random.choice(
+            data.shape[0], size=self.n_cluster, replace=False)
+        self.centroids = np.copy(data[idx, :])
+
+    def fit(self, data):
+        """
+        Fit the model to the training dataset.
+        Args:
+            data: M x D Matrix(M data points with D attributes each)(numpy float)
+        Returns:
+            The object itself
+        """
+        if data.shape[0] < self.n_cluster:
+            raise ValueError(
+                'Number of clusters is grater than number of datapoints')
+
+        best_centroids = None
+        m_score = float('inf')
+
+        for _ in range(self.n_init):
+            self.init_centroids(data)
+
+            for _ in range(self.max_iter):
+                cluster_assign = self.e_step(data)
+                old_centroid = np.copy(self.centroids)
+                self.m_step(data, cluster_assign)
+
+                if np.abs(old_centroid - self.centroids).sum() < self.delta:
+                    break
+
+            cur_score = self.evaluate(data)
+
+            if cur_score < m_score:
+                m_score = cur_score
+                best_centroids = np.copy(self.centroids)
+
+        self.centroids = best_centroids
+
+        return self
+
+    def e_step(self, data):
+        """
+        Expectation Step.
+        Finding the cluster assignments of all the points in the data passed
+        based on the current centroids
+        Args:
+            data: M x D Matrix (M training samples with D attributes each)(numpy float)
+        Returns:
+            Cluster assignment of all the samples in the training data
+            (M) Vector (M number of samples in the train dataset)(numpy int)
+        """
+        #TODO
+        distance=[]
+        op=[]
+        for i in range(len(data)):
+            for j in self.centroids:
+                distance.append(np.linalg.norm(data[i]-j))
+            op.append(distance.index(min(distance)))
+            distance.clear()
+        return op
+
+    def m_step(self, data, cluster_assgn):
+        """
+        Maximization Step.
+        Compute the centroids
+        Args:
+            data: M x D Matrix(M training samples with D attributes each)(numpy float)
+        Change self.centroids
+        """
+        #TODO
+        js={}
+        for i in range(len(self.centroids)):
+            js[cluster_assgn[i]] = []
+        for i in range(len(cluster_assgn)):
+            js[cluster_assgn[i]].append(data[i])
+        for i in js:
+            a= np.average(js[i],axis=0)
+            self.centroids[i]=a
+
+    def evaluate(self, data):
+        """
+        K-Means Objective
+        Args:
+            data: Test data (M x D) matrix (numpy float)
+        Returns:
+            metric : (float.)
+        """
+        #TODO
+        distance=[]
+        metric=0
+        for i in range(len(data)):
+            for j in range(len(self.centroids)):
+                distance.append((self.centroids[j]-data[i])*(self.centroids[j]-data[i]))
+        distance = np.sum(distance, axis=1)
+        for i in distance: 
+            metric+=i
+        return metric
